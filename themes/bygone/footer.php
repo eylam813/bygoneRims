ac0fca0e51da74251c4dc2cf49678e4bace758c5
Emily <lam.emily813@gmail.com>
2020-02-10 02:59:52 -0500

    Added footer sections to footer.php. Started styling, doesnt work yet.

--- themes/bygone/footer.php
+++ themes/bygone/footer.php
@@ -14,17 +14,18 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		<div id="social-media-icons">Social</div>
+		<div id="quick-links">
+			<div id="foot-menu-1" class="foot-menu">1</div>
+			<div id="foot-menu-2" class="foot-menu">2</div>
+			<div id="foot-menu-3" class="foot-menu">3</div>
+			<div id="foot-menu-4" class="foot-menu">4</div>
+		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bygone' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bygone' ), 'WordPress' );
-				?>
-			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bygone' ), 'bygone', '<a href="https://emilylam.ca">Emily Lam</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bygone' ), 'bygone', 'Emily | Ajay | Vasu' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
