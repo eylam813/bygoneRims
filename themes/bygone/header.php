ac8a72d29f2eedd763cee489276f8726c0eeab9f
Emily <lam.emily813@gmail.com>
2020-02-10 01:29:06 -0500

    Layed out header elements in a row.

--- themes/bygone/header.php
+++ themes/bygone/header.php
@@ -28,7 +28,7 @@
 		<div class="site-branding">
 
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bygone' ); ?></button>
+				<!--<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bygone' ); ?></button>-->
 				<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
