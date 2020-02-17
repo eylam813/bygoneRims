b162fdef0d1155053a1d3302ea9890e3729c3e87
Ajay Dubey <2beyajay@gmail.com>
2020-02-17 16:55:53 -0500

    created the woocommerce.php file, included in functions.php.  Integrated mini cart for header.php

--- themes/bygone/functions.php
+++ themes/bygone/functions.php
@@ -230,6 +230,9 @@ require get_template_directory() . '/inc/template-functions.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
+// loading the woocommerce.php file with a require
+require get_template_directory() . '/inc/woocommerce.php';
+
 /**
  * Load Jetpack compatibility file.
  */
