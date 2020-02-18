d50a3f944d1f047b8d5a15a2507d7d577a587fd3
Ajay Dubey <2beyajay@gmail.com>
2020-02-18 18:38:18 -0500

    deleted the navigation.js file

--- themes/bygone/functions.php
+++ themes/bygone/functions.php
@@ -176,8 +176,6 @@ function bygone_scripts()
 {
 	wp_enqueue_style('bygone-style', get_stylesheet_uri());
 
-	wp_enqueue_script('bygone-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
-
 	wp_enqueue_script('bygone-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
