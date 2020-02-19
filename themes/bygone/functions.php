86d6a7bbc22124e70c06a89ef20b36801a213835
Ajay Dubey <2beyajay@gmail.com>
2020-02-18 19:24:48 -0500

    deleted the require for custom header in function.php

--- themes/bygone/functions.php
+++ themes/bygone/functions.php
@@ -205,10 +205,6 @@ function bygone_scripts()
 }
 add_action('wp_enqueue_scripts', 'bygone_scripts');
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
