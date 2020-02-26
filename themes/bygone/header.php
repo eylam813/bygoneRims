8b0e42c69b150e25bfb2595cc3e4e6747437285e
Emily <lam.emily813@gmail.com>
2020-02-26 14:18:53 -0500

    Temp added responsive menu code. Not working yet.

--- themes/bygone/header.php
+++ themes/bygone/header.php
@@ -27,6 +27,36 @@
 		<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'bygone'); ?></a> -->
 
 		<header id="masthead" class="site-header grid-x">
+			<!-- hamburger menu -->
+			<div class="title-bar" data-responsive-toggle="bygone-menu" data-hide-for="medium">
+				<button class="menu-icon" type="button" data-toggle="bygone-menu"></button>
+				<div class="title-bar-title">Menu</div>
+			</div>
+
+			<div class="top-bar" id="bygone-menu">
+				<div class="top-bar-left">
+					<ul class="dropdown menu" data-dropdown-menu>
+					<li class="menu-text">Site Title</li>
+					<li>
+						<a href="#">One</a>
+						<ul class="menu vertical">
+						<li><a href="#">One</a></li>
+						<li><a href="#">Two</a></li>
+						<li><a href="#">Three</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Two</a></li>
+					<li><a href="#">Three</a></li>
+					</ul>
+				</div>
+				<div class="top-bar-right">
+					<ul class="menu">
+					<li><input type="search" placeholder="Search"></li>
+					<li><button type="button" class="button">Search</button></li>
+					</ul>
+				</div>
+			</div>
+
 
 			<div class="large-4">
 			<?php
