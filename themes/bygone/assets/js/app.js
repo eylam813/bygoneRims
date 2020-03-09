78c03c97affff47b1b157e139da953ae1f410dff
Ajay Dubey <2beyajay@gmail.com>
2020-03-09 01:48:28 -0400

    fixed the header hamburger menu, loaded the app.js file in functions.php

--- themes/bygone/assets/js/app.js
+++ themes/bygone/assets/js/app.js
@@ -1,19 +1,34 @@
-$(document).foundation()
+// $(document).foundation()
 
 
 let hamburgerMenu = document.querySelector("#bygone-hamburger-menu.hamburger-menu")
 
+
+
+window.onload = function () {
+    let hamburgerButton = document.querySelector("#masthead .hamburger-wrapper #hamburger-icon");
+    
+    hamburgerButton.addEventListener("click", function () {
+        showHamburgerMenu();
+        console.log("hello");
+    })
+}
+
+
 function showHamburgerMenu() {
     // let x = hamburgerMenu;
-    var x = document.querySelector("#bygone-hamburger-menu.hamburger-menu");
+    // let x = document.querySelector("#bygone-hamburger-menu .hamburger-menu");
+    let x = document.querySelector("#bygone-hamburger-menu");
+    console.log(x);
+    
+    console.log(x.style.display);
+    
+
     if (x.style.display == "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }
-document.querySelector("#masthead .hamburger-wrapper #hamburger-icon").addEventListener("click", function () {
-    showHamburgerMenu();
-    console.log("hello")
-})
+
 
--- themes/bygone/header.php
+++ themes/bygone/header.php
@@ -27,32 +27,37 @@
 		<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'bygone'); ?></a> -->
 
 		<header id="masthead" class="site-header grid-x">
-			<div class="hamburger-wrapper small-2"> <!-- hamburger menu-->
-			<!-- <div class="hamburger-wrapper small-2" onclick="showHamburgerMenu()"> -->
+			<div class="hamburger-wrapper small-2">
+				<!-- hamburger menu-->
+				<!-- <div class="hamburger-wrapper small-2" onclick="showHamburgerMenu()"> -->
 				<button id="hamburger-icon" type="button" data-toggle="bygone-menu">menu</button>
 				<!-- <div class="title-bar-title">Menu</div> -->
-			</div>	
+			</div>
 
-			<div class="large-4 large-offset-0 medium-3 medium-offset-0 small-offset-1 small-5"> <!-- logo holder -->
+			<div class="large-4 large-offset-0 medium-3 medium-offset-0 small-offset-1 small-5">
+				<!-- logo holder -->
 				<div id="custom-logo-container">
 					<?php the_custom_logo(); ?>
 				</div>
 			</div>
 
-			<div class="large-8 medium-9 small-6"> <!-- menu holder -->
-				<div id="nav-top-menu" class="cell bygone-header-menu"> <!-- top menu -->
+			<div class="large-8 medium-9 small-6">
+				<!-- menu holder -->
+				<div id="nav-top-menu" class="cell bygone-header-menu">
+					<!-- top menu -->
 					<?php
-						wp_nav_menu(array(
-							'theme_location' => 'Header Upper',
-							// 'theme_location' => 'header-upper',
-							'menu_id'        => 'header-upper',
-							// 'link_before' => '<div class="header-top">',
-							// 'link_after' => '</div>',
-							'container_class' => 'nav-menu-cont'
-						));
+					wp_nav_menu(array(
+						'theme_location' => 'Header Upper',
+						// 'theme_location' => 'header-upper',
+						'menu_id'        => 'header-upper',
+						// 'link_before' => '<div class="header-top">',
+						// 'link_after' => '</div>',
+						'container_class' => 'nav-menu-cont'
+					));
 					?>
 				</div>
-				<div id="nav-lower-menu" class="cell bygone-header-menu"> <!-- lower menu -->
+				<div id="nav-lower-menu" class="cell bygone-header-menu">
+					<!-- lower menu -->
 					<?php
 					wp_nav_menu(array(
 						'theme_location' => 'Header Lower',
@@ -68,33 +73,35 @@
 
 
 		</header><!-- #masthead -->
+
 		<div class="mini-cart-wrapper">
 			<?php woocommerce_mini_cart(); ?>
 		</div>
-		<div class="hamburger-menu" id="bygone-hamburger-menu">
-					<?php
-						if ( has_nav_menu( 'Header Upper')){
-							$args = array(
-								'theme_location' => 'Header Upper',
-								'fallback_cb'     => 'true',
-								// 'menu_id'        => 'hamburger-header-upper',
-								'menu_class' => 'hamburger-menu',
-								'container' => ''
-							);
-							wp_nav_menu($args);
-						}
-						if ( has_nav_menu( 'Header Lower')){
-							$args = array(
-								'theme_location' => 'Header Lower',
-								'fallback_cb'     => 'true',
-								// 'menu_id'        => 'hamburger-header-lower',
-								'menu_class' => 'hamburger-menu',
-								'container' => ''
-							);
-							wp_nav_menu($args);
-						}
-					?>
-				</div>
+
+		<div class="hamburger-menu" id="bygone-hamburger-menu" style="display: none;">
+			<?php
+			if (has_nav_menu('Header Upper')) {
+				$args = array(
+					'theme_location' => 'Header Upper',
+					'fallback_cb'     => 'true',
+					// 'menu_id'        => 'hamburger-header-upper',
+					'menu_class' => 'hamburger-menu',
+					'container' => ''
+				);
+				wp_nav_menu($args);
+			}
+			if (has_nav_menu('Header Lower')) {
+				$args = array(
+					'theme_location' => 'Header Lower',
+					'fallback_cb'     => 'true',
+					// 'menu_id'        => 'hamburger-header-lower',
+					'menu_class' => 'hamburger-menu',
+					'container' => ''
+				);
+				wp_nav_menu($args);
+			}
+			?>
+		</div>
 
 
 
