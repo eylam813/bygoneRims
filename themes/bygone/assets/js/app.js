1e9ecf743f783bdacb0c382abab01ee64284ccac
Ajay Dubey <2beyajay@gmail.com>
2020-03-10 17:10:46 -0400

    got the foundation working in the js file

--- themes/bygone/assets/js/app.js
+++ themes/bygone/assets/js/app.js
@@ -1,34 +1,37 @@
-// $(document).foundation()
+jQuery(document).ready(function ($) {
+
+    $(document).foundation()
 
 
-let hamburgerMenu = document.querySelector("#bygone-hamburger-menu.hamburger-menu")
+    let hamburgerMenu = document.querySelector("#bygone-hamburger-menu.hamburger-menu")
 
 
 
-window.onload = function () {
-    let hamburgerButton = document.querySelector("#masthead .hamburger-wrapper #hamburger-icon");
-    
-    hamburgerButton.addEventListener("click", function () {
-        showHamburgerMenu();
-        console.log("hello");
-    })
-}
-
-
-function showHamburgerMenu() {
-    // let x = hamburgerMenu;
-    // let x = document.querySelector("#bygone-hamburger-menu .hamburger-menu");
-    let x = document.querySelector("#bygone-hamburger-menu");
-    console.log(x);
-    
-    console.log(x.style.display);
-    
-
-    if (x.style.display == "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+    window.onload = function () {
+        let hamburgerButton = document.querySelector("#masthead .hamburger-wrapper #hamburger-icon");
+        
+        hamburgerButton.addEventListener("click", function () {
+            showHamburgerMenu();
+            console.log("hello");
+        })
     }
-}
+
+
+    function showHamburgerMenu() {
+        // let x = hamburgerMenu;
+        // let x = document.querySelector("#bygone-hamburger-menu .hamburger-menu");
+        let x = document.querySelector("#bygone-hamburger-menu");
+        console.log(x);
+        
+        console.log(x.style.display);
+        
+
+        if (x.style.display == "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+});
 
 
