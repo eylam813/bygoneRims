71d28cdaca2e66d465417cfedcf9141501877ab1
Ajay Dubey <2beyajay@gmail.com>
2020-03-29 00:12:20 -0400

    created template-part php file for events

---
+++ themes/bygone/template-parts/content-bygone_theme_event.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part for displaying page events in single.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bygone
+ */
+
+?>
+
+
+<?php 
+    echo the_title();
+    
+?>
