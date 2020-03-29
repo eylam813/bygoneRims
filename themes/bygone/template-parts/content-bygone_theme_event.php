cba7911e7cd6ea2fcac22dfd09f245f0ab281ed2
Ajay Dubey <2beyajay@gmail.com>
2020-03-29 03:24:14 -0400

    styled events archive and single page

--- themes/bygone/template-parts/content-bygone_theme_event.php
+++ themes/bygone/template-parts/content-bygone_theme_event.php
@@ -18,11 +18,13 @@ if (is_singular("bygone_theme_event")) {
 
     <div class="grid-x grid-container event-container">
 
-        <div class="one-event">
-            <h2 class="large-10 large-offset-1"><?php the_title(); ?></h2>
-            <img src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
-            <?php echo the_content() ?>
-            
+        <div class="one-event grid-x">
+            <h2 class="large-8 large-offset-2 small-12"><?php the_title(); ?></h2>
+            <?php if (has_post_thumbnail()) { ?>
+                <img class="large-12 event-featured-image" src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
+            <?php }
+            echo the_content() ?>
+
         </div>
 
     </div>
@@ -32,11 +34,16 @@ if (is_singular("bygone_theme_event")) {
 ?>
     <div class="grid-x grid-container events-container">
 
-        <div class="one-event">
-            <h2 class="large-10 large-offset-1"><?php the_title(); ?></h2>
-            <img src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
-            <p><?php echo get_the_excerpt() ?></p>
-            <a href=" <?php echo get_permalink() ?>">Read More: </a>
+        <div class="one-event grid-x grid-margin-x">
+            <h2 class="cell large-8 large-offset-2 small-12"><?php the_title(); ?></h2>
+            <?php if (has_post_thumbnail()) { ?>
+                <img class="cell large-3 large-offset-1 small-12 event-featured-image" src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                <p class="cell large-8 small-12"><?php echo get_the_excerpt() ?></p>
+            <?php } else { ?>
+
+                <p class="cell large-12 small-12"><?php echo get_the_excerpt() ?></p>
+            <?php } ?>
+            <a class="cell large-offset-10 large-2 events-read-more" href=" <?php echo get_permalink() ?>">Read More: </a>
         </div>
 
     </div>
