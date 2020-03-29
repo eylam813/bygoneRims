60a0ed4f5e5f6ee94fc139b5dc88d41eaf584a3f
Ajay Dubey <2beyajay@gmail.com>
2020-03-29 01:38:33 -0400

    got the singular and multiple conditional working for events template part; fixed classes and id madness in footer.php

--- themes/bygone/template-parts/content-bygone_theme_event.php
+++ themes/bygone/template-parts/content-bygone_theme_event.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying page events in single.php
  *
@@ -10,7 +11,34 @@
 ?>
 
 
-<?php 
-    echo the_title();
-    
+<?php
+
+if (is_singular("bygone_theme_event")) {
 ?>
+
+    <div class="grid-x grid-container event-container">
+
+        <div class="one-event">
+            <h2 class="large-10 large-offset-1"><?php the_title(); ?></h2>
+            <img src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
+            <?php echo the_content() ?>
+            
+        </div>
+
+    </div>
+
+<?php
+} else {
+?>
+    <div class="grid-x grid-container events-container">
+
+        <div class="one-event">
+            <h2 class="large-10 large-offset-1"><?php the_title(); ?></h2>
+            <img src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
+            <p><?php echo get_the_excerpt() ?></p>
+            <a href=" <?php echo get_permalink() ?>">Read More: </a>
+        </div>
+
+    </div>
+<?php
+}
