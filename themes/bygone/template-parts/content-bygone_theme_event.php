8f87a9706ba5c0d2f8dabef312a2daa5b4073206
Ajay Dubey <2beyajay@gmail.com>
2020-03-29 03:41:07 -0400

    fixed the padding and spacing for events-container

--- themes/bygone/template-parts/content-bygone_theme_event.php
+++ themes/bygone/template-parts/content-bygone_theme_event.php
@@ -38,12 +38,14 @@ if (is_singular("bygone_theme_event")) {
             <h2 class="cell large-8 large-offset-2 small-12"><?php the_title(); ?></h2>
             <?php if (has_post_thumbnail()) { ?>
                 <img class="cell large-3 large-offset-1 small-12 event-featured-image" src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
-                <p class="cell large-8 small-12"><?php echo get_the_excerpt() ?></p>
+                <p class="cell large-7 small-12"><?php echo get_the_excerpt() ?></p>
+                <div class="large-1"></div>
             <?php } else { ?>
 
                 <p class="cell large-12 small-12"><?php echo get_the_excerpt() ?></p>
             <?php } ?>
-            <a class="cell large-offset-10 large-2 events-read-more" href=" <?php echo get_permalink() ?>">Read More: </a>
+            
+            <a class="cell large-offset-10 large-2 small-5 small-offset-7 events-read-more" href=" <?php echo get_permalink() ?>">Read More: </a>
         </div>
 
     </div>
