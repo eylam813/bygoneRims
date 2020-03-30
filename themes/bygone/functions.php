94799be693383448b5578912d516ed7250caaf74
Vasu Sukhija <vasusukhija61@gmail.com>
2020-03-30 17:47:22 -0400

    register a shop sidebar in functions.php

--- themes/bygone/functions.php
+++ themes/bygone/functions.php
@@ -171,6 +171,15 @@ function bygone_widgets_init()
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	));
+	register_sidebar(array(
+		'name'          => esc_html__('Shop Sidebar', 'bygone'),
+		'id'            => 'shopSidebar',
+		'description'   => esc_html__('Add widgets here.', 'bygone'),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	));
 }
 add_action('widgets_init', 'bygone_widgets_init');
 
