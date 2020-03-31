37265632bb668871f86d2453b95872acd74d2ba5
Ajay Dubey <2beyajay@gmail.com>
2020-03-31 01:53:43 -0400

    wrapping the connect with us line inside the conditional for social links

--- themes/bygone/footer.php
+++ themes/bygone/footer.php
@@ -19,45 +19,47 @@
 
 
 	<!-- latest 3 "event" post type for the footer -->
-	
+
 	<div class="grid-x grid-container footer-events">
 		<?php
-			$args = array(
-				'post_type' => 'bygone_theme_event',
-				'posts_per_page' => 3,
-			);
-			// the query
-			$events = new WP_Query($args);
+		$args = array(
+			'post_type' => 'bygone_theme_event',
+			'posts_per_page' => 3,
+		);
+		// the query
+		$events = new WP_Query($args);
 
-			if ($events->have_posts()) :
-				// the loop
-				while ($events->have_posts()) : 
-					$events->the_post(); 
+		if ($events->have_posts()) :
+			// the loop
+			while ($events->have_posts()) :
+				$events->the_post();
 		?>
-					<div class="one-footer-event">
-						<h2 class="large-10 large-offset-1"><?php the_title(); ?></h2>
-						<img src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
-						<p><?php echo get_the_excerpt() ?></p>
-						<a href=" <?php echo get_permalink() ?>">Read More: </a>
-					</div>
+				<div class="one-footer-event">
+					<h2 class="large-10 large-offset-1"><?php the_title(); ?></h2>
+					<img src=" <?php echo get_the_post_thumbnail_url(); ?>" alt="">
+					<p><?php echo get_the_excerpt() ?></p>
+					<a href=" <?php echo get_permalink() ?>">Read More: </a>
+				</div>
 			<?php endwhile; ?>
 			<!-- end of the loop -->
-			<?php else : ?>
-				<p><?php esc_html__( 'Sorry, currently no events available.' ); ?></p>
-			<?php endif; ?>
-		</div>
+		<?php else : ?>
+			<p><?php esc_html__('Sorry, currently no events available.'); ?></p>
+		<?php endif; ?>
+	</div>
 
-	<!-- decorative footer line -->
-	<hr id="footer-line">
-	<!-- social media footer section -->
-	<div id="social-media-icons-container" class="grid-x grid-padding-x large-12 medium-12 small-12">
-		<div>
-			<h3 id="social-title" class="reset">Connect With Us</h3>
-		</div>
-		<div id="social-media-icons-inner-container">
+	<?php if (get_theme_mod('bygone_facebook_url') || get_theme_mod('bygone_twitter_url') || get_theme_mod('bygone_instagram_url')) { ?>
+		<!-- decorative footer line -->
+		<hr id="footer-line">
 
-			<!-- social links -->
-			<?php if (get_theme_mod('bygone_facebook_url') || get_theme_mod('bygone_twitter_url') || get_theme_mod('bygone_instagram_url')) { ?>
+		<!-- social media footer section -->
+		<div id="social-media-icons-container" class="grid-x grid-padding-x large-12 medium-12 small-12">
+			<div>
+				<h3 id="social-title" class="reset">Connect With Us</h3>
+			</div>
+			<div id="social-media-icons-inner-container">
+
+				<!-- social links -->
+
 				<div class="social-media">
 					<?php if (get_theme_mod('bygone_facebook_url')) { ?>
 
@@ -74,34 +76,35 @@
 						<div class="facebook social-icon"><a href=" <?php echo get_theme_mod('bygone_twitter_url'); ?> "> <img src="<?php echo get_template_directory_uri() . '/assets/img/twitter.svg'; ?>" alt="<?php echo esc_html__('Twitter'); ?>"></a></div>
 					<?php } ?>
 				</div>
-			<?php } ?>
 
-			<?php
-			// wp_nav_menu(array(
-			// 	'theme_location' => 'footer-social',
-			// 	'menu_class' => 'row add-child-border',
-			// ));
-			?>
+
+				<?php
+				// wp_nav_menu(array(
+				// 	'theme_location' => 'footer-social',
+				// 	'menu_class' => 'row add-child-border',
+				// ));
+				?>
+			</div>
 		</div>
-	</div>
+	<?php } ?>
 	<!-- quick links section -->
 	<div id="quick-links" class="grid-x large-12 medium-12 small-12">
 		<div class="cell">
 			<h3 id="quick-links-title">Quick Links</h3>
 		</div>
 		<?php
-			if ( has_nav_menu( 'footer-menu-1' ) ) {
-				wp_nav_menu( array( 'theme_location' => 'footer-menu-1' ) );
-			}
-			// if ( has_nav_menu(' footer-menu-1 ') ) {
-			// 	$args = array(
-			// 		'theme_location' => 'footer-menu-1',
-			// 		'container' => '',
-			// 	);
-			// 	wp_nav_menu( $args );
-			// }
+		if (has_nav_menu('footer-menu-1')) {
+			wp_nav_menu(array('theme_location' => 'footer-menu-1'));
+		}
+		// if ( has_nav_menu(' footer-menu-1 ') ) {
+		// 	$args = array(
+		// 		'theme_location' => 'footer-menu-1',
+		// 		'container' => '',
+		// 	);
+		// 	wp_nav_menu( $args );
+		// }
 		?>
-		
+
 		<div id="foot-menu-1" class="foot-menu large-3 medium-3 small-6">
 			<?php
 			wp_nav_menu(array(
